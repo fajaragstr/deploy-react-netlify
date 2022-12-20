251fc2097005d286a30bd5b72302e1fbe0712971
Fajar Dwi Agustiar <fajardwagstr@gmail.com>
2022-12-20 08:53:40 +0700

    test continous deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>fajaragstr</h2>
+        <p>Test continous deployment...</p>
         <a
           className="App-link"
           href="https://reactjs.org"
